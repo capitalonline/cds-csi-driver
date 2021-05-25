7bab32cd36a271d7ef7b4b9823d10c237c0d1eb6
yusong.he <yusong.he@capitalonline.net>
2021-05-25 17:10:53 +0800

    优化oss传输速度

--- pkg/driver/oss/const.go
+++ pkg/driver/oss/const.go
@@ -1,8 +1,9 @@
 package oss
 
 const (
-	AuthTypeDefault = "saveAkFile"
-	defaultOssRoot    = "/"
-	CredentialFile = "/etc/s3pass"
-	defaultOtherOpts = "-o dbglevel=info -o curldbg -o allow_other -o use_path_request_style"
+	AuthTypeDefault  = "saveAkFile"
+	defaultOssRoot   = "/"
+	CredentialFile   = "/etc/s3pass"
+	defaultOtherOpts = "-o dbglevel=info -o curldbg -o allow_other -o use_path_request_style " +
+		"-o umask=0 -o max_write=131072 -o big_writes -o enable_noobj_cache -o use_cache=/dev/shm -o sigv2 -o del_cache"
 )
