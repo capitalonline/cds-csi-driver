1f99231eb00a648cc28a20b0f5e6ece23f872012
yusong.he <yusong.he@capitalonline.net>
2021-05-26 17:51:09 +0800

    添加oss挂载日志信息

--- pkg/driver/utils/utils.go
+++ pkg/driver/utils/utils.go
@@ -86,7 +86,8 @@ func (n *NodeMeta) GetNodeID() string {
 
 // Mounted checks whether a volume is mounted
 func Mounted(mountPath string) bool {
-	cmd := fmt.Sprintf("mount | grep %s | grep -v grep | wc -l", mountPath)
+	//cmd := fmt.Sprintf("mount | grep %s | grep -v grep | wc -l", mountPath)
+	cmd := fmt.Sprintf("df -h | grep %s | grep -v grep | wc -l", mountPath)
 	out, err := RunCommand(cmd)
 	if err != nil {
 		log.Infof("check whether mounted exec error: %s, %s", cmd, err.Error())
