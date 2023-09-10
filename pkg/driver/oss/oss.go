4438b249f3d526c4cc33e9502b5dd2bb7a5965e1
junqiang <junqiang.wang@qq.com>
2023-09-11 07:00:44 +0800

    fix bug

--- pkg/driver/oss/oss.go
+++ pkg/driver/oss/oss.go
@@ -1,12 +1,8 @@
 package oss
 
 import (
-	"fmt"
 	"github.com/capitalonline/cds-csi-driver/pkg/common"
 	"github.com/kubernetes-csi/drivers/pkg/csi-common"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func NewDriver(driverName, nodeId, endpoint string) *OssDriver {
@@ -21,19 +17,5 @@ func NewDriver(driverName, nodeId, endpoint string) *OssDriver {
 func (d *OssDriver) Run() {
 	s := csicommon.NewNonBlockingGRPCServer()
 	s.Start(d.endpoint, d.idServer, nil, d.nodeServer)
-	//s.Wait()
-
-	signals := make(chan os.Signal)
-
-	// Notify signals to channel
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
-
-	// Block until a signal is received
-	fmt.Println("Waiting for signal...")
-	sig := <-signals
-	fmt.Println("Received signal:", sig)
-
-	// Exit program
-	fmt.Println("Exiting program")
-	os.Exit(0)
+	s.Wait()
 }
