e91571833ecf64823626948dd689926ca8e54722
junqiang <junqiang.wang@qq.com>
2023-09-11 06:43:13 +0800

    fix bug

--- pkg/driver/oss/nodeserver.go
+++ pkg/driver/oss/nodeserver.go
@@ -79,7 +79,7 @@ func (n *NodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublish
 
 	var mntCmd string
 	log.Debugf("NodePublishVolume:: Start mount source [%s:%s] to [%s]", opts.Bucket, opts.Path, opts.NodePublishPath)
-	mntCmd = fmt.Sprintf("sh -c \"s3fs %s:%s %s -o passwd_file=%s -o url=%s %s\"", opts.Bucket, opts.Path, opts.NodePublishPath, CredentialFile, opts.URL, defaultOtherOpts)
+	mntCmd = fmt.Sprintf("s3fs %s:%s %s -o passwd_file=%s -o url=%s %s", opts.Bucket, opts.Path, opts.NodePublishPath, CredentialFile, opts.URL, defaultOtherOpts)
 	log.Debugf("mntCmd is: %s", mntCmd)
 	//if _, err := utils.RunCommand(mntCmd); err != nil {
 	//	log.Errorf("Mount oss bucket to mountPath failed, error is: %s", err)
