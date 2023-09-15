adec86244436b784e1e822891ec23ea45fe3c469
junqiang <junqiang.wang@qq.com>
2023-09-15 08:06:38 +0800

    fix bug

--- pkg/driver/oss/const.go
+++ pkg/driver/oss/const.go
@@ -1,8 +1,8 @@
 package oss
 
 const (
-	AuthTypeDefault = "saveAkFile"
-	defaultOssRoot    = "/"
-	CredentialFile = "/etc/s3pass"
+	AuthTypeDefault  = "saveAkFile"
+	defaultOssRoot   = "/"
+	CredentialFile   = "/host/etc/s3pass"
 	defaultOtherOpts = "-o dbglevel=info -o curldbg -o allow_other -o use_path_request_style"
 )
