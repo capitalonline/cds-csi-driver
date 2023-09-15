bd964730f6e81204aee3e780d3db671804b0c9b7
junqiang.wang <junqiang.wang@mail.com>
2023-09-15 14:48:01 +0800

    fix bug

--- dist/install-oss.sh
+++ dist/install-oss.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-cp -rf /oss-server /host/config/bin/
-cp -rf /oss.service /host/config/system/
+cp -rf /oss-server /host/config/bin/oss-server
+cp -rf /oss.service /host/config/system/oss.service
 
 nsenter -m -u -i -p -F -t 1 sh -c "systemctl enable oss.service"
 nsenter -m -u -i -p -F -t 1 sh -c "systemctl start oss.service"
