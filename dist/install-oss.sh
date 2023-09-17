84d9c45b91d65869ca2e54aaa57901a389bd37d9
dengke.tang <dengke.tang@capitalonline.net>
2023-09-18 00:32:18 +0800

    update oss.service

--- dist/install-oss.sh
+++ dist/install-oss.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-cp -rf /oss-server /srv/oss-server
-cp -rf /oss.service /host/config/system/oss.service
+cp -f /oss-server /srv/oss-server
+cp -f /oss.service /host/config/system/oss.service
 
-nsenter -m -u -p -F -t 1 sh -c "systemctl enable oss.service"
-nsenter -m -u -p -F -t 1 sh -c "systemctl start oss.service"
+nsenter -m -u -p -F -t 1 sh -c "systemctl daemon-reload; systemctl enable oss.service; systemctl start oss.service"
