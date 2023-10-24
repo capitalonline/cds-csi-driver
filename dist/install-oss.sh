60000318ed05ea836aab1c3232aecd802d00209d
junqiang <junqiang.wang@outlook.com>
2023-10-24 14:08:02 +0800

    check s3fs tool

--- dist/install-oss.sh
+++ dist/install-oss.sh
@@ -3,4 +3,10 @@
 cp -f /oss-server /srv/oss-server
 cp -f /oss.service /host/config/system/oss.service
 
-nsenter -m -u -p -F -t 1 sh -c "systemctl daemon-reload; systemctl enable oss.service; systemctl start oss.service"
+code=`nsenter -m -u -p -F -t 1 sh -c "(which s3fs || ((which apt && apt install s3fs -y) || (which yum && yum install s3fs -y))) && systemctl daemon-reload && systemctl enable oss.service && systemctl start oss.service"`
+if [ $? -eq 0 ]; then
+    exit 0
+fi
+
+echo "failed to install s3fs"
+exit -1
