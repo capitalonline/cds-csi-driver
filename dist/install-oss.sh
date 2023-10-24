493cdc0d652e507f68475b5bac4cf27e76a3f011
junqiang <junqiang.wang@outlook.com>
2023-10-24 13:53:47 +0800

    fix bug

--- dist/install-oss.sh
+++ dist/install-oss.sh
@@ -3,7 +3,7 @@
 cp -f /oss-server /srv/oss-server
 cp -f /oss.service /host/config/system/oss.service
 
-code=nsenter -m -u -p -F -t 1 sh -c "(which s3fs || ((which apt && apt install s3fs -y) || (which yum && yum install s3fs -y))) && systemctl daemon-reload && systemctl enable oss.service && systemctl start oss.service"
+code=`nsenter -m -u -p -F -t 1 sh -c "(which s3fs || ((which apt && apt install s3fs -y) || (which yum && yum install s3fs -y))) && systemctl daemon-reload && systemctl enable oss.service && systemctl start oss.service"`
 if [ $? -eq 0 ]; then
     exit 0
 fi
