55e6643e954cb25b1760fb8484a93da1bbd43e5b
junqiang <junqiang.wang@outlook.com>
2023-10-24 12:51:04 +0800

    fix bug

--- dist/install-oss.sh
+++ dist/install-oss.sh
@@ -3,4 +3,9 @@
 cp -f /oss-server /srv/oss-server
 cp -f /oss.service /host/config/system/oss.service
 
-nsenter -m -u -p -F -t 1 sh -c "systemctl daemon-reload; systemctl enable oss.service; systemctl start oss.service"
+nsenter -m -u -p -F -t 1 sh -c "stemctl daemon-reload; systemctl enable oss.service; systemctl start oss.service"
+
+if [ $? -eq 0 ]; then
+    echo "111111"
+fi
+echo "2222222"
